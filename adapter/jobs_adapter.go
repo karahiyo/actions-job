2ff0710f2abc044c61a07d801ea57302b08aa95a
Yusuke Shimizu yu_ke <karahiyo@users.noreply.github.com>
2023-06-11 16:39:00 +0900

    use instancemetadata (#13)
    * use instancemetadata

--- adapter/jobs_adapter.go
+++ adapter/jobs_adapter.go
@@ -14,50 +14,48 @@ import (
 )
 
 type JobsAdapter interface {
-	GetJob(ctx context.Context, project, name string) (*run.Job, error)
-	CreateJob(ctx context.Context, project string, job *run.Job) (*run.Job, error)
-	UpdateJob(ctx context.Context, project, name string, job *run.Job) (*run.Job, error)
-	StartJob(ctx context.Context, project, name string) (*run.Execution, error)
-	WaitJobReady(ctx context.Context, project, name string) (bool, error)
+	GetJob(ctx context.Context, name string) (*run.Job, error)
+	CreateJob(ctx context.Context, job *run.Job) (*run.Job, error)
+	UpdateJob(ctx context.Context, name string, job *run.Job) (*run.Job, error)
+	StartJob(ctx context.Context, name string) (*run.Execution, error)
+	WaitJobReady(ctx context.Context, name string) (bool, error)
 }
 
 type jobsAdapter struct {
-	api *run.APIService
+	api     *run.APIService
+	project string
+	region  string
 }
-
-var globalRunAdminAPIEndpoint string = "https://run.googleapis.com"
 
 var ErrJobNotFound = errors.New("job not found")
 
-func NewJobsAdapter(ctx context.Context, region string) (JobsAdapter, error) {
+func NewJobsAdapter(ctx context.Context, project, region string) (JobsAdapter, error) {
 	var opts []option.ClientOption
-	if region == "" || region == "global" {
-		opts = append(opts, option.WithEndpoint(globalRunAdminAPIEndpoint))
-	} else {
-		opts = append(opts, option.WithEndpoint(fmt.Sprintf("https://%s-run.googleapis.com", region)))
-	}
+	opts = append(opts, option.WithEndpoint(fmt.Sprintf("https://%s-run.googleapis.com", region)))
 
 	s, err := run.NewService(ctx, opts...)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize jobs v1 api jobsAdapter: %w", err)
 	}
 	return &jobsAdapter{
-		api: s,
+		project: project,
+		region:  region,
+		api:     s,
 	}, nil
 }
 
-func (a *jobsAdapter) CreateJob(ctx context.Context, project string, job *run.Job) (*run.Job, error) {
-	parent := fmt.Sprintf("namespaces/%s", project)
+func (a *jobsAdapter) CreateJob(ctx context.Context, job *run.Job) (*run.Job, error) {
+	parent := fmt.Sprintf("namespaces/%s", a.project)
 	res, err := a.api.Namespaces.Jobs.Create(parent, job).Do()
 	if err != nil {
-		return nil, fmt.Errorf("failed to CreateJob: project=%s, job=%s, %w", project, spew.Sdump(job), err)
+		return nil, fmt.Errorf("failed to CreateJob: job=%s, %w", spew.Sdump(job), err)
 	}
 
 	return res, nil
 }
 
-func (a *jobsAdapter) UpdateJob(ctx context.Context, project, name string, job *run.Job) (*run.Job, error) {
-	jobID := fmt.Sprintf("namespaces/%s/jobs/%s", project, name)
+func (a *jobsAdapter) UpdateJob(ctx context.Context, name string, job *run.Job) (*run.Job, error) {
+	jobID := fmt.Sprintf("namespaces/%s/jobs/%s", a.project, name)
 	res, err := a.api.Namespaces.Jobs.ReplaceJob(jobID, job).Do()
 	if err != nil {
 		var gErr *googleapi.Error
@@ -65,14 +63,14 @@ func (a *jobsAdapter) UpdateJob(ctx context.Context, project, name string, job *
 			return nil, fmt.Errorf("job dose not found: name=%s, %w", jobID, ErrJobNotFound)
 		}
 
-		return nil, fmt.Errorf("failed to ReplaceJob: project=%s, name=%s, job=%s, %w", project, name, spew.Sdump(job), err)
+		return nil, fmt.Errorf("failed to ReplaceJob: name=%s, job=%s, %w", name, spew.Sdump(job), err)
 	}
 
 	return res, nil
 }
 
-func (a *jobsAdapter) GetJob(ctx context.Context, project, name string) (*run.Job, error) {
-	jobID := fmt.Sprintf("namespaces/%s/jobs/%s", project, name)
+func (a *jobsAdapter) GetJob(ctx context.Context, name string) (*run.Job, error) {
+	jobID := fmt.Sprintf("namespaces/%s/jobs/%s", a.project, name)
 	job, err := a.api.Namespaces.Jobs.Get(jobID).Do()
 	if err != nil {
 		var gErr *googleapi.Error
@@ -80,14 +78,14 @@ func (a *jobsAdapter) GetJob(ctx context.Context, project, name string) (*run.Jo
 			return nil, fmt.Errorf("job dose not found: name=%s, %w", jobID, ErrJobNotFound)
 		}
 
-		return nil, fmt.Errorf("failed to get job: project=%s, name=%s, %w", project, name, err)
+		return nil, fmt.Errorf("failed to get job: name=%s, %w", name, err)
 	}
 
 	return job, nil
 }
 
-func (a *jobsAdapter) StartJob(ctx context.Context, project, name string) (*run.Execution, error) {
-	jobID := fmt.Sprintf("namespaces/%s/jobs/%s", project, name)
+func (a *jobsAdapter) StartJob(ctx context.Context, name string) (*run.Execution, error) {
+	jobID := fmt.Sprintf("namespaces/%s/jobs/%s", a.project, name)
 	runJobRequest := &run.RunJobRequest{
 		// FIXME: Using the 'namespaces.jobs.run' api with a container override option will result in a 403 permission denied error, even through a service account that has the run.jobs.runWithOverrides permission.
 		Overrides: nil,
@@ -100,30 +98,30 @@ func (a *jobsAdapter) StartJob(ctx context.Context, project, name string) (*run.
 			return nil, fmt.Errorf("job dose not found: name=%s, %w", jobID, ErrJobNotFound)
 		}
 
-		return nil, fmt.Errorf("failed to start job: project=%s, name=%s, req=%s, res=%s, %w", project, name, spew.Sdump(runJobRequest), spew.Sdump(execution), err)
+		return nil, fmt.Errorf("failed to start job: name=%s, req=%s, res=%s, %w", name, spew.Sdump(runJobRequest), spew.Sdump(execution), err)
 	}
 
 	return execution, nil
 }
 
 // WaitJobReady Wait until the job's Ready status condition is True
-func (a *jobsAdapter) WaitJobReady(ctx context.Context, project, name string) (bool, error) {
+func (a *jobsAdapter) WaitJobReady(ctx context.Context, name string) (bool, error) {
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 
 	logger := zerolog.Ctx(ctx)
-	logger.Debug().Msgf("waiting for the job to be ready: project=%s, name=%s", project, name)
+	logger.Debug().Msgf("waiting for the job to be ready: name=%s", name)
 
 	nextWait := 10 * time.Millisecond
 	for {
-		job, err := a.GetJob(ctx, project, name)
+		job, err := a.GetJob(ctx, name)
 		if err != nil {
-			return false, fmt.Errorf("failed to get job: project=%s, name=%s, %w", project, name, err)
+			return false, fmt.Errorf("failed to get job: name=%s, %w", name, err)
 		}
 
 		for _, condition := range job.Status.Conditions {
 			if condition.Type == "Ready" && condition.Status == "True" {
-				logger.Debug().Msgf("job ready: project=%s, name=%s", project, name)
+				logger.Debug().Msgf("job ready: name=%s", name)
 				return true, nil
 			}
 		}
---
+++ adapter/metadata.go
@@ -0,0 +1,88 @@
+package adapter
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+const endpoint = "http://metadata.google.internal"
+
+type Metadata struct {
+	ProjectID string
+	Region    string
+}
+
+func GetInstanceMetadata(ctx context.Context) (*Metadata, error) {
+	meta := new(Metadata)
+	metadataClient, err := NewMetadataClient()
+	if err != nil {
+		return nil, fmt.Errorf("failed to NewMetadataClient: %w", err)
+	}
+
+	if meta.ProjectID, err = metadataClient.getProjectID(ctx); err != nil {
+		return nil, fmt.Errorf("failed to getProjectID: %w", err)
+	}
+
+	if meta.Region, err = metadataClient.getRegion(ctx); err != nil {
+		return nil, fmt.Errorf("failed to getRegion: %w", err)
+	}
+
+	return meta, nil
+}
+
+type MetadataClient struct {
+	cli *http.Client
+}
+
+func NewMetadataClient() (*MetadataClient, error) {
+	cli := &http.Client{
+		Timeout: 1 * time.Second,
+	}
+
+	return &MetadataClient{cli: cli}, nil
+}
+
+func (m *MetadataClient) getProjectID(ctx context.Context) (string, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("%s/computeMetadata/v1/project/project-id", endpoint), nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create request: %w", err)
+	}
+	req.Header.Add("Metadata-Flavor", "Google")
+
+	resp, err := m.cli.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to get region: %w", err)
+	}
+	defer resp.Body.Close() // nolint:errcheck
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	return string(body), nil
+}
+
+func (m *MetadataClient) getRegion(ctx context.Context) (string, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("%s/computeMetadata/v1/instance/region", endpoint), nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create request: %w", err)
+	}
+	req.Header.Add("Metadata-Flavor", "Google")
+
+	resp, err := m.cli.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to get region: %w", err)
+	}
+	defer resp.Body.Close() // nolint:errcheck
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	return string(body), nil
+}
