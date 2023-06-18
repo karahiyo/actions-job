4bcfc9bd546bc247e4e4dcd6a18950c7d8c879ac
Yusuke Shimizu <yusuke.shimizu@legalontech.jp>
2023-06-18 17:20:16 +0900

    fix image tags

--- docker/runner/entrypoint-dind-rootless.sh
+++ docker/runner/entrypoint-dind-rootless.sh
@@ -20,6 +20,17 @@ if [ -n "${DOCKER_REGISTRY_MIRROR}" ]; then
 jq ".\"registry-mirrors\"[0] = \"${DOCKER_REGISTRY_MIRROR}\"" /home/runner/.config/docker/daemon.json > /tmp/.daemon.json && mv /tmp/.daemon.json /home/runner/.config/docker/daemon.json
 fi
 
+if [ -d /home/runner/.local ]; then
+  if [ ! -d /home/runner/.local/share ]; then
+    log.notice "Creating /home/runner/.local/share owned by runner:runner \
+so that rootless dockerd will not fail with a permission error when creating /home/runner/.local/share/docker"
+
+    sudo mkdir /home/runner/.local/share
+    sudo chmod 755 /home/runner/.local/share
+    sudo chown runner:runner /home/runner/.local/share
+  fi
+fi
+
 echo "Start Docker daemon (rootless)"
 export DOCKERD_ROOTLESS_ROOTLESSKIT_FLAGS=--debug
 export DOCKERD_ROOTLESS_ROOTLESSKIT_NET=slirp4netns
